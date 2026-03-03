c434a2d1082241dad83291bfde32454bf29e1873
Finoana Rasolomalala <118520988+FinoanaRas@users.noreply.github.com>
2026-03-04 00:14:02 +0300

    try to fix route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'client/product/:id', // Specific rule for the dynamic route
+    renderMode: RenderMode.Server  // Render on-demand, not at build time
+  },
+  {
+    path: 'client/shop/:id', // Specific rule for the dynamic route
+    renderMode: RenderMode.Server  // Render on-demand, not at build time
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
